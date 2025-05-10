855124a23976fa955fe295c6454929734a801f4d
arcademiss <georgeventel97@gmail.com>
2025-05-10 21:28:27 +0300

    last fix before starting on features

--- src/main/java/com/product_comparator/productcomparator/model/Product.java
+++ src/main/java/com/product_comparator/productcomparator/model/Product.java
@@ -1,13 +1,12 @@
 package com.product_comparator.productcomparator.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 
 
-@Entity
+
+
 public class Product {
 
-    @Id
+
     private String productId;
     private String productName;
     private String productCategory;
