1deeff0f4e38c0542a6336c4693a28adbd87df70
arcademiss <georgeventel97@gmail.com>
2025-05-10 18:30:18 +0300

    added setters getters and constructors

--- src/main/java/com/product_comparator/productcomparator/model/PriceHistory.java
+++ src/main/java/com/product_comparator/productcomparator/model/PriceHistory.java
@@ -10,4 +10,37 @@ public class PriceHistory {
     private String productId;
     private LocalDate date;
     private double price;
+
+    public PriceHistory() {
+    }
+
+    public PriceHistory(String productId, LocalDate date, double price) {
+        this.productId = productId;
+        this.date = date;
+        this.price = price;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
--- src/main/java/com/product_comparator/productcomparator/model/UserAlert.java
+++ src/main/java/com/product_comparator/productcomparator/model/UserAlert.java
@@ -9,4 +9,38 @@ public class UserAlert {
     private String productId;
     private double targetPrice;
     private String email;
+
+    public UserAlert(String productId, double targetPrice, String email) {
+        this.productId = productId;
+        this.targetPrice = targetPrice;
+        this.email = email;
+    }
+
+    public UserAlert() {
+
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public double getTargetPrice() {
+        return targetPrice;
+    }
+
+    public void setTargetPrice(double targetPrice) {
+        this.targetPrice = targetPrice;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
