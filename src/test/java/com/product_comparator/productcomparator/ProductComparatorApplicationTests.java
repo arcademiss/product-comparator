644d398ef532268d45be209b0287876c82727e45
arcademiss <georgeventel97@gmail.com>
2025-05-10 16:54:37 +0300

    Initial commit

---
+++ src/test/java/com/product_comparator/productcomparator/ProductComparatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.product_comparator.productcomparator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductComparatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
