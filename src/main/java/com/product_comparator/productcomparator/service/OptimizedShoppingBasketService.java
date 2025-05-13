15880e010238de568534648bd8c704178750a167
arcademiss <georgeventel97@gmail.com>
2025-05-13 08:32:37 +0300

    Started on OptimizedShoppingBasketService

---
+++ src/main/java/com/product_comparator/productcomparator/service/OptimizedShoppingBasketService.java
@@ -0,0 +1,22 @@
+package com.product_comparator.productcomparator.service;
+
+import com.product_comparator.productcomparator.dto.BasketItemInputDto;
+import com.product_comparator.productcomparator.dto.OptimizedShoppingBasketOutputDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class OptimizedShoppingBasketService {
+    public OptimizedShoppingBasketOutputDto getOptimizedBasket(List<BasketItemInputDto> items, LocalDate date) {
+        // TODO Get data from database for products and discounts
+        // TODO Create discounted items
+        // TODO Optimize the shopping basket
+        // TODO Create object for return
+        return OptimizedShoppingBasketOutputDto.builder().build();
+    }
+
+}
