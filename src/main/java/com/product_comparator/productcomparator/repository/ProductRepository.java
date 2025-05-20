4f1de305af2d86912ed3c5d2be2d72d4023ce8ef
arcademiss <georgeventel97@gmail.com>
2025-05-20 11:19:21 +0300

    added specification to repository and created spec class

--- src/main/java/com/product_comparator/productcomparator/repository/ProductRepository.java
+++ src/main/java/com/product_comparator/productcomparator/repository/ProductRepository.java
@@ -2,6 +2,7 @@ package com.product_comparator.productcomparator.repository;
 
 import com.product_comparator.productcomparator.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -11,9 +12,9 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Integer> {
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
     boolean existsByProductIdAndStoreAndDate(String productId, String store, LocalDate date);
-    List<Product> findByProductName(String name);
+
     List<Product> findByProductNameContainingIgnoreCase(String name);
     @Query(
             value = "SELECT * FROM product WHERE product_id = :productId " +
---
+++ src/main/java/com/product_comparator/productcomparator/repository/ProductSpecification.java
@@ -0,0 +1,30 @@
+package com.product_comparator.productcomparator.repository;
+
+import com.product_comparator.productcomparator.entity.Product;
+import org.springframework.data.jpa.domain.Specification;
+
+public class ProductSpecification {
+    public static Specification<Product> hasName(String name) {
+        return (root, query, cb) -> name == null || name.isBlank()
+                ? null
+                : cb.equal(cb.lower(root.get("productName")), name.toLowerCase());
+    }
+
+    public static Specification<Product> hasStore(String store) {
+        return (root, query, cb) -> store == null || store.isBlank()
+                ? null
+                : cb.equal(cb.lower(root.get("productStore")), store.toLowerCase());
+    }
+
+    public static Specification<Product> hasBrand(String brand) {
+        return (root, query, cb) -> brand == null || brand.isBlank()
+                ? null
+                : cb.equal(cb.lower(root.get("productBrand")), brand.toLowerCase());
+    }
+
+    public static Specification<Product> hasCategory(String category) {
+        return (root, query, cb) -> category == null || category.isBlank()
+                ? null
+                : cb.equal(cb.lower(root.get("productCategory")), category.toLowerCase());
+    }
+}
