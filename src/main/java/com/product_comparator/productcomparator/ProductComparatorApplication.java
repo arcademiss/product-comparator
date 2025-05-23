5a03c9f0203e42456ea7d3dc263be23b28262dde
arcademiss <georgeventel97@gmail.com>
2025-05-23 10:52:56 +0300

    cleaned DataLoader and app

--- src/main/java/com/product_comparator/productcomparator/ProductComparatorApplication.java
+++ src/main/java/com/product_comparator/productcomparator/ProductComparatorApplication.java
@@ -1,13 +1,29 @@
 package com.product_comparator.productcomparator;
 
+import com.product_comparator.productcomparator.util.DataLoader;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
+import java.io.IOException;
+
+@EnableScheduling
 @SpringBootApplication
 public class ProductComparatorApplication {
-
+    @Autowired
+    private DataLoader dataLoader;
     public static void main(String[] args) {
         SpringApplication.run(ProductComparatorApplication.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void init() throws IOException {
+        dataLoader.init();
+        System.out.println("Don1!");
     }
 
 }
